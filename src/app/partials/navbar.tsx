6c07ceeb21f621d5b7bbd05787dee54ff5d81a8b
rinarudhei <rinaldiadrian5@gmail.com>
2026-02-19 06:49:06 +0700

    feat: navbar sm screen

--- src/app/partials/navbar.tsx
+++ src/app/partials/navbar.tsx
@@ -12,9 +12,21 @@ import Link from 'next/link';
 
 export const Navbar = () => {
   return (
-    <nav className='flex-between mt-4 h-12 w-90.25 rounded-full bg-black/20 px-4 text-white backdrop-blur-2xl'>
+    <nav className='flex-between mt-4 h-12 w-full max-w-[calc(100%-2rem)] gap-6 rounded-full bg-black/20 px-4 text-white backdrop-blur-2xl sm:mt-8 sm:w-fit sm:max-w-138.25 sm:px-6'>
+      <ul className='sm:flex-center hidden gap-6'>
+        {navigationData.map((data) => (
+          <li key={data.label} className='gap-2 p-2'>
+            <Link
+              href={data.href}
+              className='hover:text-primary-200 text-md font-medium'
+            >
+              {data.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
       <Dialog>
-        <DialogTitle className='text-xl font-bold text-white'>
+        <DialogTitle className='text-xl font-bold text-white sm:hidden'>
           Edwin
         </DialogTitle>
         <DialogTrigger className='cursor-pointer sm:hidden'>
