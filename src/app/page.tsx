acbaaec84d9d93745658c45dd7d4cda45128adf2
rinarudhei <rinaldiadrian5@gmail.com>
2026-02-18 16:52:17 +0700

    chore: add button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
-import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div className=''>
-      <main className='font-regular text-display-md'>
-        <h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-          laboriosam{' '}
-          <span className='font-bonheur-royale'>nostrum maiores magni</span>{' '}
-          minima quidem voluptate, sint quia tempora? Veritatis, et doloribus?
-          Suscipit voluptatem cum laudantium distinctio. Quaerat, omnis
-          accusantium.
-        </h1>
-      </main>
+    <div className='h-90 w-screen border border-black'>
+      <Button className='flex'>TEST</Button>
     </div>
   );
 }
---
+++ src/components/ui/button.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
+import { Slot } from 'radix-ui';
+
+import { cn } from '@/lib/utils';
+
+const buttonVariants = cva(
+  'flex justify-center items-center shrink-0 disabled:pointer-events-none disabled:opacity-50 cursor-pointer outline-none',
+  {
+    variants: {
+      variant: {
+        default:
+          'bg-primary-300 text-white hover:bg-primary-300/90 font-bold text-sm rounded-full',
+      },
+      size: {
+        default: 'h-11 p-2 gap-1',
+        lg: 'h-12 p-2 gap-1',
+        icon: 'size-9',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  }
+);
+
+function Button({
+  className,
+  variant = 'default',
+  size = 'default',
+  asChild = false,
+  ...props
+}: React.ComponentProps<'button'> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+  }) {
+  const Comp = asChild ? Slot.Root : 'button';
+
+  return (
+    <Comp
+      data-slot='button'
+      data-variant={variant}
+      data-size={size}
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  );
+}
+
+export { Button, buttonVariants };
