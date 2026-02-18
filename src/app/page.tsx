ffa2abbbc34de2c37d1886b157b27197eda680bc
rinarudhei <rinaldiadrian5@gmail.com>
2026-02-18 07:54:08 +0700

    chore: add custom tailwindcss variables and utilities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,14 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className=''>
-      <main className=''></main>
+      <main className='font-regular text-display-md'>
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+          laboriosam nostrum maiores magni minima quidem voluptate, sint quia
+          tempora? Veritatis, et doloribus? Suscipit voluptatem cum laudantium
+          distinctio. Quaerat, omnis accusantium.
+        </h1>
+      </main>
     </div>
   );
 }
